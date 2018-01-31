90ab2dab6bcc68d9ce616a011f23b5b2bca9f9b4
Gil <gino.ladowitch@gmail.com>
2018-01-31 13:34:27 +0100

    0.7 - Game is working! Editor ok, added a config file.

---
+++ config.py
@@ -0,0 +1,53 @@
+# MAIN_2D
+
+TILE_W = 35
+TILE_H = 35
+
+ASSETS_PATH = "assets/"
+
+# MAIN_2D / Musics and sound effects:
+TITLE_MUSIC = ASSETS_PATH + "sounds/title_music.ogg"
+GAME_MUSIC = ASSETS_PATH + "sounds/game_music.ogg"
+ITEM_SOUND = ASSETS_PATH + "sounds/item_sound.ogg"
+WIN_MUSIC = ASSETS_PATH + "sounds/win_music.ogg"
+LOSS_MUSIC = ASSETS_PATH + "sounds/loss_music.ogg"
+
+# MAIN_2D / Graphics:
+WELCOME = ASSETS_PATH + "welcome.png"
+WIN = ASSETS_PATH + "win.png"
+LOSS = ASSETS_PATH + "loss.png"
+
+PATH = ASSETS_PATH + "path.png"
+WALL = ASSETS_PATH + "wall.png"
+
+ITEM_B = ASSETS_PATH + "item_b.png"
+ITEM_S = ASSETS_PATH + "item_s.png"
+ITEM_N = ASSETS_PATH + "item_n.png"
+
+GUARDIAN = ASSETS_PATH + "guardian.png"
+
+HERO_UP = ASSETS_PATH + "up.png"
+HERO_DOWN = ASSETS_PATH + "down.png"
+HERO_LEFT = ASSETS_PATH + "left.png"
+HERO_RIGHT = ASSETS_PATH + "right.png"
+
+
+# EDITOR
+
+BASE = [
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,3],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+[1,4,1,1,1,1,1,1,1,1,1,1,1,1,1],
+]
